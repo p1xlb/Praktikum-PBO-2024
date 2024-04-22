0d312e58408a473a443e5ec6e728759a222e3207
p1xl <p1xl@mememachinethefourth>
2024-04-23 01:08:18 +0800

    PT5 UP

---
+++ PT5/src/Maskapai/mekanik.java
@@ -0,0 +1,51 @@
+package Maskapai;
+
+final public class mekanik extends karyawan {
+    private String id_mekanik;
+    private String spesialisasi;
+    
+    public mekanik(String id_mekanik, String nama, String rating) {
+        super(nama, rating);
+        setId_mekanik(id_mekanik);
+        this.spesialisasi = "General";
+    }
+    
+    public mekanik(String id_mekanik, String nama, String rating, String spesialisasi) {
+        super(nama, rating);
+        setId_mekanik(id_mekanik);
+        this.spesialisasi = spesialisasi;
+    }
+
+    public void setId_mekanik(String id_mekanik) {
+        if (id_mekanik == null || id_mekanik.isEmpty()) {
+            throw new IllegalArgumentException("ID mekanik tidak bisa kosong.");
+        }
+        if (!id_mekanik.startsWith("M")) {
+            throw new IllegalArgumentException("ID mekanik harus diawali dengan 'M'.");
+        }
+        this.id_mekanik = id_mekanik;
+    }
+
+    public String getId_mekanik() {
+        return id_mekanik;
+    }
+
+    public String getSpesialisasi() {
+        return spesialisasi;
+    }
+
+    public void setSpesialisasi(String spesialisasi) {
+        this.spesialisasi = spesialisasi;
+    }
+
+    public void DefSpesialisasi(String spesialisasi) {
+        this.spesialisasi = "General";
+    }
+
+    @Override
+    public final String getDetails() {
+        return "ID Mekanik: " + id_mekanik + "\nNama Mekanik: " + getNama() + "\nRating: " + getRating() + "\nSpesialisasi: " + spesialisasi;
+    }
+
+
+}
